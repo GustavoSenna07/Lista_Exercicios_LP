3c58d2e175e8db9c7cad199017765c2c9f723544
GustavoSenna07 <gustavo.psenna@gmail.com>
2025-05-08 10:02:50 -0300

    Exercicios 3, 4 e 6 da Lista 5 corrigidos

--- Listas de Exercícios/LP_Lista_05/Lista5_ex3/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Lista5_ex3/src/Main.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        /*
+        versão Gustavo
+
         int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int userNumber, check = 0;
 
@@ -21,6 +25,22 @@ public class Main {
         else {
             System.out.println("O numero " + userNumber + " não esta na lista");
         }
+        */
 
+        int [] vetor = {2, 4, 6, 3, 9, 0, 23, 87};
+        int n;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite um numero inteiro: ");
+        n = in.nextInt();
+
+        for (int i = 0; i < vetor.length ; i++) {
+            if(vetor[i]==n) {
+                System.out.println(n + " Encontrado na posição " + i);
+                return;
+            }
+        }
+
+        System.out.println("Valor não encontrado");
     }
 }
